c90fc7d7907a0e6cd950373e543e6b25367335ab
Christian Gawron <christian.gawron@gmail.com>
2010-06-10 23:54:03 +0200

    Comment cleanup

--- src/de/cgawron/agoban/EditSGF.java
+++ src/de/cgawron/agoban/EditSGF.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2010 Christian Gawron
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cgawron.agoban;
 
 import de.cgawron.go.Goban;
@@ -8,6 +24,9 @@ import de.cgawron.go.SimpleGoban;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * Provides an sgf editor.
+ */
 public class EditSGF extends Activity
 {
     /** Called when the activity is first created. */
