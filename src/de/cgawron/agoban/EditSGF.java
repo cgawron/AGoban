6a2413eaf7ff64ee8a7c85ccb0e34ee46dcce286
Christian Gawron <christian.gawron@gmail.com>
2010-06-16 22:32:06 +0200

    Added SeekBar to scroll through GameTree

--- src/de/cgawron/agoban/EditSGF.java
+++ src/de/cgawron/agoban/EditSGF.java
@@ -32,14 +32,17 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SeekBar;
 
 /**
  * Provides an sgf editor.
  */
-public class EditSGF extends Activity
+public class EditSGF extends Activity implements SeekBar.OnSeekBarChangeListener
 {
     public static Resources resources;
     private GobanView gobanView;
+    private SeekBar seekBar;
+    private GameTree gameTree;
 
     /** Called when the activity is first created. */
     @Override
@@ -50,18 +53,23 @@ public class EditSGF extends Activity
         setContentView(R.layout.main);
 
 	gobanView = (GobanView) findViewById(R.id.goban);
+	seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+	seekBar.setOnSeekBarChangeListener(this);
     }
     
     @Override
     public void onStart() {
 	super.onStart();
-	GameTree gameTree = null;
+	gameTree = null;
 
 	try {
 	    gameTree = new GameTree(new StringReader("(;FF[4]DT[2004-12-06]EV[Nikolaus Meschede]HA[0]KM[1,5]RE[B+5]RO[3]SZ[9]GM[1]FF[4];B[cc];W[gf];B[dg];W[ce];B[de];W[df];B[cf];W[ef];B[dd];W[cg];B[bf];W[bg];B[be];W[ec];B[ee];W[fe];B[dc];W[eb];B[db];W[ed];B[eg];W[ff];B[fg];W[gh];B[gg];W[hg];B[fh];W[hh];B[ch];W[fi];B[ei];W[dh];B[gi];W[hi];B[di];W[bh];B[eh];W[fi];B[bi];W[da];B[ca];W[ea];B[hc];W[gc];B[gb];W[gd];B[he];W[hd];B[ic];W[id];B[ha];W[ib];B[fc];W[fd];B[fb];W[hb];B[gi];W[ai];B[ah];W[fi];B[];W[gi])"));
-	    Node leaf = gameTree.getLeafs().get(0);
-	    Goban goban = (MarkupModel) leaf.getGoban();
 	    
+	    seekBar.setMax(gameTree.getNoOfMoves());
+	    seekBar.setKeyProgressIncrement(1);
+
+	    Goban goban = (MarkupModel) gameTree.getRoot().getGoban();
 	    gobanView.setGoban(goban);
 	}
 	catch (Exception ex) {
@@ -70,4 +78,21 @@ public class EditSGF extends Activity
 
     }
 
+    @Override
+    public void onProgressChanged(SeekBar  seekBar, int moveNo, boolean fromUser) {
+	Log.d("EditSGF", "move " + moveNo);
+	Node moveNode = gameTree.getMove(moveNo);
+	if (moveNode != null) {
+	    Goban goban = (MarkupModel) moveNode.getGoban();
+	    gobanView.setGoban(goban);
+	}
+    }
+    
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
 }
